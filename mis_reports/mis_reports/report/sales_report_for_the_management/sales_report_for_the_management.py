eee078d2f023a9e7243ac84e75182b481a1518d1
Your Name <you@example.com>
2019-02-23 14:38:22 +0530

    added total in reports

--- mis_reports/mis_reports/report/sales_report_for_the_management/sales_report_for_the_management.py
+++ mis_reports/mis_reports/report/sales_report_for_the_management/sales_report_for_the_management.py
@@ -13,7 +13,7 @@ def execute(filters=None):
 	return columns,data
 
 def get_column():
-	return [_("Date") + ":Date:90",_("Category") + ":Data:90",_("Subcategory") + ":Data:110",_("Size") + ":Data:90",_("Color") + ":Data:105",_("Sales Invoice") + ":Data:110",_("Qty") + ":Data:105",_("Rate") + ":Data:105",_("Discount ") + ":Data:105",_("Cash Margin ") + ":Data:105",_("Margin % ") + ":Data:105",_("Discount ") + ":Data:105"]
+	return [_("Date") + ":Date:90",_("Category") + ":Data:90",_("Subcategory") + ":Data:110",_("Size") + ":Data:90",_("Color") + ":Data:105",_("Sales Invoice") + ":Data:110",_("Qty") + ":Float:105",_("Rate") + ":Float:105",_("Discount %") + ":Float:105"]
 
 def get_data(conditions,filters):
 	sales = frappe.db.sql("""SELECT si.posting_date, i.item_group, SUBSTRING_INDEX(i.item_name, '-', 1),SUBSTRING_INDEX(SUBSTRING_INDEX(i.item_name, '-', 2), '-', -1),SUBSTRING_INDEX(i.item_name, '-', -1), si.name, i.qty, i.rate, i.discount_percentage from `tabSales Invoice Item` i, `tabSales Invoice` si where si.name = i.parent %s;""" %conditions,filters, as_list=1)
